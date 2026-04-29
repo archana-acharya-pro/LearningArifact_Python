b8ae69595c095ad97864e08c4a189d851391765b
Archana Acharya <archana.acharya.pro@gmail.com>
2026-04-29 10:30:28 -0400

    HelloWorld & Colab test

---
+++ src/hello_world/hello_world.py
@@ -0,0 +1,10 @@
+
+print("Hello, World!")
+
+name = input("What's your name? ")
+
+print("Hello {}!\nWelcome to Dataquest!".format(name))
+
+name = input("What's your name? ")
+
+print("Hello {}!\nI am siri. Who are you?!".format(name))
